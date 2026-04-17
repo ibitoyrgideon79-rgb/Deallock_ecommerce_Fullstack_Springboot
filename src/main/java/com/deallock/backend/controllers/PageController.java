00d5b2fbbf74da95f69aefa2f8be539181504ba2
Gideon Ibitoye <ibitoyrgideon79@gmail.com>
2026-04-17 10:42:14 +0100

    fix hard coded user dashboard and make it dynamic

--- src/main/java/com/deallock/backend/controllers/PageController.java
+++ src/main/java/com/deallock/backend/controllers/PageController.java
@@ -35,7 +35,7 @@ public class PageController {
 
     @GetMapping("/ourteam")
     public String ourteam() {
-        return "ourteam";
+        return "/contactus";
     }
 
     @GetMapping("/contactus")
